bac045480eb125dc4f2c3587686da2667e16b7d3
Damilola Bada <90965007+damygoes@users.noreply.github.com>
2022-07-14 21:32:49 +0200

    added content-security-policy to the metatag in the pubilc folder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { VscActivateBreakpoints } from "react-icons/vsc";
 import "./App.css";
 
 function App() {
-	const BASE_URL = "https://www.boredapi.com/api/activity?type=";
+	const BASE_URL = "http://www.boredapi.com/api/activity?type=";
 
 	const [activity, setActivity] = useState([]);
 	const [userSelectValue, setUserSelectValue] = useState("");
